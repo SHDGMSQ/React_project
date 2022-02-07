dd47dfc3351499696f610951db948c51063202dc
SHDGMSQ <sh.dg.msq@mail.ru>
2022-02-07 15:34:43 +0300

    after 02 video

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,65 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      This is APP component
-        <Rating />
-        <Accordion/>
+    console.log("App rendering")
+    return (
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
+}
+
+function AppTitle () {
+    console.log("AppTitle rendering")
+    return <>This is APP component</>
+}
+
+function Rating() {
+    console.log("Rating rendering")
+    return (
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
+    )
+}
+
+function Accordion() {
+    console.log("Accordion rendering")
+    return <div>
+        <AccordionTitle/>
+        <AccordionBody/>
     </div>
-  );
 }
 
-function Rating () {
-  return (
-      <div>
-        <Star/>
-        <Star/>
-        <Star/>
-        <Star/>
-        <Star/>
-      </div>
-  )
-}
-
-function Accordion () {
-  return <div>
-    <h3>Menu</h3>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
-  </div>
-}
-
-function Star () {
+function Star() {
+    console.log("Star rendering")
     return <div>star</div>
 
+}
+
+function AccordionTitle () {
+    console.log("AccordionTitle rendering")
+    return <div>
+        <h3>Menu</h3>
+    </div>
+}
+
+function AccordionBody () {
+    console.log("AccordionBody rendering")
+    return <div>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
 }
 
 export default App;
