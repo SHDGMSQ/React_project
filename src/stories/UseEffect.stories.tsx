b63d1c57a18298537fe19a16901788bc4be59109
SHDGMSQ <sh.dg.msq@mail.ru>
2022-03-27 11:09:11 +0300

    after 25 video ( useEffect, cleanup)

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -35,7 +35,7 @@ export const SimpleExample = () => {
     </>
 }
 
-export const SetTimeoutExample = () => {
+export const SetIntervalExample = () => {
     console.log("SetTimeoutExample")
 
     const [fake, setFake] = useState(1)
@@ -62,6 +62,74 @@ export const SetTimeoutExample = () => {
         {/*<button onClick={() => setFake(fake + 1 )}>fake+</button>*/}
         {/*<button onClick={() => setCounter(counter + 1 )}>counter+</button>*/}
         Hello, counter: {date}
+    </>
+}
+
+
+export const ResetEffectExample = () => {
+
+
+    const [counter, setCounter] = useState(1)
+
+    console.log('Component rendered with ' + counter);
+
+    useEffect(() => {
+        console.log('Effect occurred ' + counter);
+
+        return () => {
+            console.log('RESET EFFECT ' + counter);
+        }
+        }, [counter])
+
+    const increase = () => {setCounter(counter +1)}
+        return <>
+        Hello, counter: {counter} <button onClick={increase}>+</button>
+    </>
+}
+
+
+export const KeysTrackerExample = () => {
+
+
+    const [text, setText] = useState('')
+
+    console.log('Component rendered with ' + text);
+
+        useEffect( () => {
+            const handler = (e: KeyboardEvent) => {
+                console.log(e.key);
+                setText(text + e.key)
+            }
+            window.document.addEventListener('keypress', handler)
+            return () => {
+                window.removeEventListener('keypress', handler)
+                }
+        }, [text])
+
+        return <>
+        Typed text: {text}
+    </>
+}
+
+export const SetTimeoutExample = () => {
+
+
+    const [text, setText] = useState('')
+
+    console.log('Component rendered with ' + text);
+
+        useEffect( () => {
+            const timeoutId = setTimeout( () => {
+                console.log('TIMEOUT EXPIRED');
+                setText('3 seconds passed')
+            }, 3000 )
+            return () => {
+                clearTimeout(timeoutId)
+                }
+        }, [text])
+
+        return <>
+        text: {text}
     </>
 }
 
